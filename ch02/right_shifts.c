45d50f5c7179a9026619bc480e6c88e6a27668d6
Carlos Galdino <carloshsgaldino@gmail.com>
2015-10-15 22:56:23 -0300

    Add `sra`.
    This function performs an arithmetic right shift using a logical right shift.

--- ch02/right_shifts.c
+++ ch02/right_shifts.c
@@ -10,11 +10,28 @@ unsigned srl(unsigned x, int k)
     return xsra & mask;
 }
 
+int sra(int x, int k)
+{
+    /* Perform shift logically */
+    int xsrl      = (unsigned) x >> k;
+    int w         = sizeof(int) << 3;
+    unsigned sign = !!((0x1 << (w - 1)) & x);
+    unsigned mask = (~sign + 1) << (w - k);
+
+    return xsrl | mask;
+}
+
 int main(int argc, char **argv)
 {
     printf("%.8x\n", srl(0x1 << 24, 8));
     printf("%.8x\n", srl(~0x0, 6));
     printf("%.8x\n", srl(0x0, 6));
+
+    printf("\n");
+
+    printf("%.8x\n", sra(0x9 << 28, 8));
+    printf("%.8x\n", sra(~0x0, 6));
+    printf("%.8x\n", sra(0x0, 6));
 
     return 0;
 }
